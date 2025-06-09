8ac0760c5e550c90af67b2bdb4de170feba72228
Isaque-Cyber <isaque_r_silva@estudante.sesisenai.org.br>
2025-06-09 16:59:27 -0300

    start

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+inicializar()
+
+
+
+
+let usuario = {
+    username: '',
+    email: '',
+    password: ''
+}
+function cadastrar() {
+    usuario.nome = document.getElementById('Cnome').value
+    usuario.email = document.getElementById('Cemail').value
+    usuario.senha = document.getElementById('Csenha').value
+    alert("Cadastrado com sucesso!! :D")
+
+    console.log(usuario)
+
+    mostrarLogin()
+
+}
+
+function logar() {
+    let nome = document.getElementById('Lnome').value
+    let senha = document.getElementById('Lsenha').value
+
+    if ((nome === usuario.nome || nome === usuario.email) && senha === usuario.senha) {
+        alert("Login efetuado com sucesso!")
+        limparInputs()
+        mostrarProdutos()
+        // document.getElementById('navbar').style.display = 'block'
+    } else {
+        alert("Login não efetuado sem sucesso!")
+    }
+}
+
+function mostrarCadastro() {
+    esconderBotoes()
+    document.getElementById('Cadastro').style.display = 'flex'
+    document.getElementById('Cnome').focus()
+}
+
+
+
+
+
+function inicializar() {
+    mostrarCadastro()
+}
 
