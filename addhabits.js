7694d0dbb92849c6c407cddbe02d4a6b0762e5e2
ISAQUE RIBEIRO DA SILVA <isaque_r_silva@estudante.sesisenai.org.br>
2025-06-23 22:10:14 -0300

    habits new

---
+++ addhabits.js
@@ -0,0 +1,50 @@
+function Adicionar(){
+    window.location.href = "addhabits.html"
+}
+
+
+function addHabit(){
+    
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+habits = {
+    sugestoes = ['Beber água',
+        'Exercitar-se',
+        'Ler um livro',
+        'Meditar',
+        'Dormir cedo',
+        'Estudar',
+        'Fazer um esporte',
+        'Praticar yoga',
+        'Fazer uma caminhada',
+        'Desenhar',
+        'Tocar um instrumento']
+
+        
+}
+
+
+todos = [...habits.sugestoes, ...habits.usuario]
