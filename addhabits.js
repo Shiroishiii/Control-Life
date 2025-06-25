d1ab1781d948f6fcd3f0f79a9dae22f90651c962
ISAC BARBOSA DOS SANTOS <isac_b_santos@estudante.sesisenai.org.br>
2025-06-24 22:20:56 -0300

    isac

--- addhabits.js
+++ addhabits.js
@@ -1,10 +1,29 @@
-function Adicionar(){
-    window.location.href = "addhabits.html"
+const userSelectedSugHabit = localStorage.getItem('suggested-habit-selected')
+if(userSelectedSugHabit){
+    input = document.getElementById("habit-name")
+    input.value = userSelectedSugHabit
+    localStorage.removeItem('suggested-habit-selected')
 }
 
-
 function addHabit(){
-    window.location.href = "habits.html"
+    let nomehabito = document.getElementById().value
+    // todos os inputs
+
+
+     }
+
+
+    const habito= {
+        nomehabito: nomehabito,
+        // todos os campos
+    }
+
+
+
+
+function addSuggestedHabit(habit){
+    localStorage.setItem("suggested-habit-selected", habit)
+    window.location.href = "addhabits.html"
 }
 
 
