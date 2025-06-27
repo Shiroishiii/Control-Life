d6e2ed97057a191541f4ad550ecff761751541df
JULIO CESAR VALVERDE GASPARETO <julio_gaspareto@estudante.sesisenai.org.br>
2025-06-26 22:06:48 -0300

    avanços no perfil

---
+++ profile.js
@@ -0,0 +1,7 @@
+
+function botarImafem(){
+var file = document.getElementById("inImg")
+var img = document.getElementById("image")
+file.addEventListener("change",(e)=>{
+    img.src = URL.createObjectURL(e.target.files[0])
+})
