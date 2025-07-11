aa0f23d827d4bd129bbffa51be3bdfa10cb6f504
ISAQUE RIBEIRO DA SILVA <isaque_r_silva@estudante.sesisenai.org.br>
2025-07-10 22:28:04 -0300

    Tela de Habitos

---
+++ MeusHabitos.js
@@ -0,0 +1,21 @@
+function mostrarCards(){
+    const habitos = JSON.parse(localStorage.getItem("habitos")) || []
+    console.log (habitos)
+
+    document.getElementById("CardsContainer").innerHTML +=''
+    for(let i=0; i < habitos.length; i++){
+        document.getElementById("CardsContainer").innerHTML +=`
+        <div id="Card">
+            <h3>${habitos[i].NomeDoHábito}</h3>
+            <p>${habitos[i].Descrição}</p>
+            <p>Meta : ${habitos[i].Meta}</p>
+            <p>Notas : ${habitos[i].Notas}</p>
+    
+        </div>
+        `
+
+    }
+
+}
+
+mostrarCards()
