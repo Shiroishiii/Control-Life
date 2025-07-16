2e278d2426d8f3d0863a484e69cff9951f8cb173
isac-barbosa <santosisac196@gmail.com>
2025-07-16 20:19:32 -0300

    fiz o grafico funcionar

--- statiscs.js
+++ statiscs.js
@@ -1,6 +1,4 @@
-let habitos = ['Água 💧', 'Estudo 📚', 'Exercício 🏃‍♂️', 'Sono 😴', "comer frutas"]
-let progressos = [80, 60, 40, 90, 44]
-let cores = ['#11FF00', '#FFF200', '#0000FF', '#FF0000', "#ff8833"]
+
 
 
 
