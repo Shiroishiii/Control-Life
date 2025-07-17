36cb23313371d0301982e61b1aa6464d834a2724
JulioGaspareto <juliogaspareto0@gmail.com>
2025-07-17 20:13:49 -0300

    Salvamento do Julio

--- MeusHabitos.js
+++ MeusHabitos.js
@@ -3,7 +3,7 @@ let contador = 0
 if(localStorage.getItem("contador")){ //Verifica se ja existe algum valor salvo
     contador = parseInt(localStorage.getItem("contador")) //parseInt converte de string para numero
 }
-document.getElementById("contador").textContent = contador //textContent serve para alterar o texto dentro de um elemento HTML
+// document.getElementById("contador").textContent = contador //textContent serve para alterar o texto dentro de um elemento HTML
 const habitos = JSON.parse(localStorage.getItem('habitos'))
 
 function add(id){
@@ -36,11 +36,23 @@ function mostrarCards(){
     console.log (habitos)
     
  
-    document.getElementById("CardsContainer").innerHTML +=''
+    // document.getElementById("CardsContainer").innerHTML +=''
     for(let i=0; i < habitos.length; i++) {
         document.getElementById("CardsContainer").innerHTML +=`
         <div class="card">
-            <h3>${habitos[i].nomeDoHabito}</h3>
+      <div class="Card">  
+                <div class="flex-linha">
+                    <h3>${habitos[i].NomeDoHábito}</h3>
+                    <div>
+                        <button class="editores" onclick="excluir(${i})">
+                            <img src="imgs/game-icons--trash-can (1).svg" alt="Excluir">
+                        </button>
+
+                        <button class="editores" onclick="editar(${i})">
+                            <img src="game-icons--pencil.svg" alt="Editar">
+                        </button>
+                    </div>
+                </div>
             <p>Descrição : ${habitos[i].descricao}</p>
             <p>Meta : ${habitos[i].meta}</p>
             <p>Notas : ${habitos[i].notas}</p>
@@ -67,35 +79,7 @@ function atualizarDadosGrafico(){
     }
 }
 
-function mostrarCards() {
-    const habitos = JSON.parse(localStorage.getItem("habitos")) || []
-    console.log(habitos)
 
-    // document.getElementById("CardsContainer").innerHTML += '' 
-    document.getElementById("CardsContainer").innerHTML = '' 
-    
-    for (let i = 0; i < habitos.length; i++) {
-        document.getElementById("CardsContainer").innerHTML += `
-            <div class="Card">  
-                <div class="flex-linha">
-                    <h3>${habitos[i].NomeDoHábito}</h3>
-                    <div>
-                        <button class="editores" onclick="excluir(${i})">
-                            <img src="imgs/game-icons--trash-can (1).svg" alt="Excluir">
-                        </button>
-
-                        <button class="editores" onclick="editar(${i})">
-                            <img src="game-icons--pencil.svg" alt="Editar">
-                        </button>
-                    </div>
-                </div>
-                <p>Descrição: ${habitos[i].Descrição}</p>
-                <p>Meta: ${habitos[i].Meta}</p>
-                <p>Notas: ${habitos[i].Notas}</p>
-            </div>
-        `
-    }
-}
 
 function excluir(index) {
     const confirmar = confirm("Tem certeza que deseja excluir este hábito?")
